ba13331597f7e8f20d034ba099b85de8e9288594
multiverseweb <multiverse.letter@gmail.com>
2024-10-26 04:34:40 +0530

    added modal

--- script.js
+++ script.js
@@ -14,3 +14,36 @@ function toggleText() {
 
 // Call the function after the DOM is fully loaded
 window.onload = toggleText;
+
+// JavaScript for modal functionality
+const modal = document.getElementById('badgeModal');
+const modalImg = document.getElementById('modalImage');
+const closeBtn = document.getElementsByClassName('close')[0];
+const images = document.querySelectorAll('.badgeImage');
+
+// When an image is clicked, open the modal
+images.forEach((img) => {
+    img.onclick = function() {
+        modal.style.display = 'block';
+        modalImg.src = this.src;
+        document.body.classList.add('modal-open');
+        document.body.style.overflow="hidden";
+    }
+});
+
+// Close the modal when the close button is clicked
+closeBtn.onclick = function() {
+    modal.style.display = 'none';
+    document.body.classList.remove('modal-open');
+    document.body.style.overflow="auto";
+}
+
+// Close the modal when clicking outside the image
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+        document.body.classList.remove('modal-open');
+        document.body.style.overflow="auto";
+    }
+}
+
